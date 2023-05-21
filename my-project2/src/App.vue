2186d8397f8ff231e480f57773f850e6ef0f2936
William yan <1337842463@qq.com>
2023-05-21 16:24:33 +0800

    final

---
+++ my-project2/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <div id="app">
+      <!--    <div id="nav">-->
+      <!--      <router-link to="/">Home</router-link>-->
+      <!--    </div>-->
+
+      <router-view/>
+    </div>
+  </v-app>
+</template>
+
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background: url('https://photo69.mac89.com/2021/01/15/10/15102628_fe285d5845_small.jpg')
+    no-repeat center center;
+    background-size: 100% 100%;
+  }
+
+  #nav {
+    padding: 30px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+      }
+  }
+</style>
